427dd65baa2cc489834e2acbbe4d58f2ef5488ee
Bilgesu Çakır <95298678+bilgesucakir@users.noreply.github.com>
2023-12-20 13:48:47 +0300

    Initial commit

---
+++ src/main/java/com/example/flightsearchapi/FlightsearchapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.flightsearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsearchapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsearchapiApplication.class, args);
+	}
+
+}
